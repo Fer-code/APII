f9eb4fbffe5f164632e320e0299f12cfe8ba45d9
CerrutiGH <66427525+CerrutiGH@users.noreply.github.com>
2021-06-15 17:49:32 -0300

    Unfortunately

--- app/src/main/java/com/example/api/MainActivity.java
+++ app/src/main/java/com/example/api/MainActivity.java
@@ -3,20 +3,68 @@ package com.example.api;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import static android.hardware.Sensor.TYPE_LIGHT;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lighEventListener;
+    private View root;
+    private float maxValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        root = findViewById(R.id.root);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(TYPE_LIGHT);
 
+        if (lightSensor == null) {
+            Toast.makeText(this, "O dispositivo não tem sensor de luminosidade", Toast.LENGTH_SHORT).show();
+            //finish();
+        }
+
+        maxValue = lightSensor.getMaximumRange();
+
+        lighEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float value = sensorEvent.values[0];
+                int newValue = (int) (255f * value / maxValue);
+                root.setBackgroundColor(Color.rgb(newValue, newValue, newValue));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+        @Override
+        protected  void onResume(){
+        super.onResume();
+        sensorManager.registerListener(lighEventListener, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(lighEventListener);
     }
 
     @Override
